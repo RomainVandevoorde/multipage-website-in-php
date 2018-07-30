3a6c87d90132b9d4c20c210339e1f11c55743a21
NathanHoutain <nathan-houtain@outlook.com>
2018-07-30 16:39:07 +0200

    lundi30headerindex

--- index.php
+++ index.php
@@ -3,8 +3,65 @@
 require __DIR__.'/templates/header.php';
 
 ?>
+<div class="ind-par ind-par-left">
+  <h2>Qu'est-ce que Solidarité Savoir ?</h2>
 
-<h2>Hello !</h2>
+  <p>Agréée et subsidiée par le Service public francophone bruxellois (COCOF) comme Centre d’Action Sociale Globale, l’association est installée depuis 2001 dans le Nord-Ouest de Bruxelles à Molenbeek-Saint-Jean.</br></br>
+
+  L’association propose plusieurs projets (que vous pouvez retrouver dans la page qui leur est dédiée) ;
+  <ul>
+    <li>une permanence sociale</li>
+    <li>des modules d’initiation à la vie citoyenne</li>
+    <li>des modules en informatique</li>
+    <li>un soutien aux femmes porteuses de projets</li>
+  </ul>
+
+  Notre approche vise à permettre à nos publics d’avoir plus de pouvoir d’action et d’influence sur leur environnement.</p>
+</div>
+
+<div class="ind-par ind-par-right">
+  <h2>Qu'est-ce qu'un Centre d'Action Sociale Globale (CASG)?</h2>
+
+  <p>
+
+Les Centres d’Action Sociale Globale (CASG) sont des centres de service social généralistes bruxellois agréés par le Service public francophone bruxellois (COCOF). Ils sont au nombre de 10. Tous sont membres de la FdSS.</br></br>
+
+Les CASG ont pour objectif d’améliorer les relations entre leurs bénéficiaires et la société. Ils tentent de favoriser l’accès de leurs bénéficiaires aux droits fondamentaux ainsi qu’aux ressources sociales, sanitaires, économiques, culturelles, etc.</br></br>
+
+    A qui s’adressent-ils ?</br></br>
+
+Les Centres d’Action Sociale Globale s’adressent à toute personne confrontée à une difficulté sociale qui en fait la demande.</br></br>
+
+    Quelles sont leurs missions ?</br></br>
+
+Les CASG développent leur action sociale globale en assurant aux bénéficiaires :</br>
+
+<ul>
+  <li>un premier accueil</li>
+  <li>une analyse de leur situation</li>
+  <li>un accompagnement social</li>
+  <li>si nécessaire, une orientation vers un service spécialisé en fonction de la problématique</li>
+</ul>
+
+En outre, les CASG relayent aux autorités publiques les problèmes et les lacunes qui se font jour dans la collectivité.</br>
+
+    Quels sont leurs modes d’intervention ?</br>
+
+Les CASG offrent une aide individuelle au travers de la permanence sociale accessible sans rendez-vous au moins 20 heures par semaine.</br>
+
+Ils mettent aussi en place des actions collectives et communautaires, afin de lutter contre les mécanismes d’exclusion sociale et contre l’isolement social. Ces activités redonnent une place d’acteur aux personnes en difficultés.</br>
+
+    Cadre légal</br>
+
+Décret du 5 mars 2009 relatif à l’offre de services ambulatoires dans les domaines de l’action sociale, de la famille et de la santé, M.B., 8 mai 2009.</br>
+
+Arrêté du Collège de la Commission communautaire française du 4 juin 2009 portant application du décret du 5 mars 2009, M.B., 22 juillet 2009.</br>
+
+Voir la Liste des CASG</br>
+
+Voir les Publications des CASG</p>
+</div>
+
 
 <?php
 
--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,35 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css">
+    <script src="assets/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
     <title>Solidarité Savoir ASBL</title>
   </head>
   <body>
+    <img src="assets/img/logo.jpg" id="header-logo" alt="Logo" height="100" width="100" >
+    <h1 id="header-title"> Solidarité Savoir ASBL </h1>
+    <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Acceuil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Projets</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Supports de Communication</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </body>
+</html>

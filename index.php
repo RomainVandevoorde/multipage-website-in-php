326e3a251946cc2416206d97f9096741971f629e
Romain Vandevoorde <romain.vandevoorde.dev@gmail.com>
2018-07-30 09:52:25 +0200

    basic structure + dependencies

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__.'/templates/header.php';
+
+?>
+
+<h2>Hello !</h2>
+
+<?php
+
+require __DIR__.'/templates/footer.php';
+
+?>
---
+++ templates/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <title>Solidarité Savoir ASBL</title>
+  </head>
+  <body>

11a3d830e833345bc79e49c4607f5115cbb79fb0
NathanHoutain <nathan-houtain@outlook.com>
2018-08-10 16:04:31 +0200

    touch

--- supcom.php
+++ supcom.php
@@ -9,14 +9,14 @@ require __DIR__.'/templates/header.php';
     <div class="row">
       <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
         <div class="card-body">
-          <h2 class="card-title proTitle"><a href="https://solidaritesavoir.files.wordpress.com/2017/05/solidaritc3a9-savoir-affiche-modules-dinitiation-c3a0-la-citoyenetc3a9.pdf" target="_blank">Affiche</a><hr>Modules d’initiation à la vie citoyenne</h2>
-          <img src="assets/img/affiche-vici.jpg" class="img-fluid mx-auto d-block" alt="Responsive image">
+          <h2 class="card-title proTitle"><a href="https://solidaritesavoir.files.wordpress.com/2017/05/solidaritc3a9-savoir-affiche-modules-en-informatique.pdf" target="_blank">Affiche</a><hr>Modules en informatique</h2>
+          <img src="assets/img/affiche-modinfo.jpg" class="img-fluid mx-auto d-block" alt="Responsive image">
         </div>
       </div>
       <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
         <div class="card-body">
-          <h2 class="card-title proTitle"><a href="https://solidaritesavoir.files.wordpress.com/2017/05/solidaritc3a9-savoir-affiche-modules-en-informatique.pdf" target="_blank">Affiche</a><hr>Modules en informatique</h2>
-          <img src="assets/img/affiche-modinfo.jpg" class="img-fluid mx-auto d-block" alt="Responsive image">
+          <h2 class="card-title proTitle"><a href="https://solidaritesavoir.files.wordpress.com/2017/05/solidaritc3a9-savoir-affiche-modules-dinitiation-c3a0-la-citoyenetc3a9.pdf" target="_blank">Affiche</a><hr>Modules d’initiation à la vie citoyenne</h2>
+          <img src="assets/img/affiche-vici.jpg" class="img-fluid mx-auto d-block" alt="Responsive image">
         </div>
       </div>
       <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">

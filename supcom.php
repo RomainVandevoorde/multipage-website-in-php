fd3c471c67bdb7912dd310538875db7a1253c06e
NathanHoutain <nathan-houtain@outlook.com>
2018-08-10 14:13:36 +0200

    fontChange

--- supcom.php
+++ supcom.php
@@ -4,28 +4,30 @@ require __DIR__.'/templates/header.php';
 
  ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
 
+      </div>
+      <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+
+      </div>
+      <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+
+      </div>
     </div>
-    <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
 
-    </div>
-    <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+      </div>
+      <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
 
+      </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
 
-    </div>
-    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
-
-    </div>
-  </div>
-
-</div>
+</body>
 
  <?php
 
--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,7 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto+Slab" rel="stylesheet"> 
     <link rel="stylesheet" type="text/css" href="assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/style/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
